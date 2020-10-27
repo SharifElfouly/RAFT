833aaafbb94bff3dd52a2419f1e0f644e2bbb552
Sharif Elfouly <selfouly@gmail.com>
2020-10-27 22:55:23 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,1 +0,0 @@
-python3 run.py --model=models/raft-things.pth --images_dir='/home/sharif/Documents/commai-challenge/data/frames/test' --output_dir='/home/sharif/Documents/commai-challenge/data/frames/oooo'
